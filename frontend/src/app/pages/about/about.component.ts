06f2068d414d02cfbc21c93c0fb83c2f45f95013
Wirlly Silva <wirlly.silva@gmail.com>
2026-07-09 00:24:51 -0300

    Adiciona pagina sobre o projeto

--- frontend/src/app/pages/about/about.component.ts
+++ frontend/src/app/pages/about/about.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { InfoPageComponent } from '../info-page.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about-page',
-  imports: [InfoPageComponent],
+  imports: [RouterModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
-export class AboutPageComponent {
-  items = ['Frontend agora em Angular.', 'Backend Java Spring Boot preservado.', 'Banco de dados serdefinido na prxima etapa.'];
-}
+export class AboutPageComponent {}
